2596365aa4fa32bb75997f01a0acc426efc5c06e
Andreas Leonard-Calcano <Darkninjawolf1@gmail.com>
2021-07-22 16:28:54 -0400

    Commented ControlComponent.js

--- timer-client/src/Components/TimerComponents/ControlsComponent.js
+++ timer-client/src/Components/TimerComponents/ControlsComponent.js
@@ -1,31 +1,40 @@
+import { auto } from "@popperjs/core";
 import React from "react";
 import { ReactDOM } from "react";
 import { Container, Row, Col, Button, FormGroup } from "reactstrap";
 import TimeSelector from "./ControlComponents/TimeSelector";
 
+/**
+ * ControlsComponent is a React Class Component.
+ * This class is intended to create and render a view of all
+ * user controls.
+ * @AndreasLc1103
+ */
 class ControlsComponent extends React.Component {
+  /**
+   *render() renders jsx to display to the user the control
+   elements of this component.
+   * @returns A genertated view 
+   */
   render() {
     return (
       <Container fluid={true}>
+        {/* Stores all the components in a single row. */}
         <Row>
-          <Col xs={6} sm={6} md={6} lg={6}>
+          {/* Creates the left most column*/}
+          <Col xs={auto} sm={6} md={6} lg={6}>
             <FormGroup>
               <TimeSelector typeSelect="workTime" />
             </FormGroup>
-          </Col>
-          <Col md={6}>
-            <FormGroup>
-              <TimeSelector typeSelect="breakTime" />
-            </FormGroup>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
             <Button color="primary" size="sm" block>
               Start
             </Button>
           </Col>
-          <Col>
+          {/* Creates the right most column*/}
+          <Col xs={auto} sm={6} md={6} lg={6}>
+            <FormGroup>
+              <TimeSelector typeSelect="breakTime" />
+            </FormGroup>
             <Button color="danger" size="sm" block>
               reset
             </Button>
